329a2e078bf8dd1688b425efece3434e37a1d08d
sumitchikane <75309655+sumitchikane@users.noreply.github.com>
2022-03-28 17:50:17 +0530

    Added useAPI hook

---
+++ useAPI/App.js
@@ -0,0 +1,14 @@
+import useApi from './useApi'
+
+const App = () => {
+  const { loading, data } = useApi('https://jsonplaceholder.typicode.com/users')
+
+  if(loading) return <h1>Loading</h1>
+
+  return <div>
+    <h1>Data fetched successfully.</h1>
+    {JSON.stringify(data)}
+  </div>
+};
+
+export default App;
